ee0754d02368d5f69a3e5299599bff856ab83fcc
palotai.robin <palotai.robin@21d757a5-8d53-0410-89ca-7355cf17ce49>
2009-07-10 08:12:13 +0000

    Development branch for swf asset tool, to be merged later

---
+++ branches/shx-branch/release.bat
@@ -0,0 +1,9 @@
+@echo off
+rm -rf release release.zip
+mkdir release
+cp -R format haxelib.xml CHANGES.txt release
+rm -rf release/*/.svn release/*/*/.svn
+7z a -tzip release.zip release
+rm -rf release
+haxelib submit release.zip
+pause
